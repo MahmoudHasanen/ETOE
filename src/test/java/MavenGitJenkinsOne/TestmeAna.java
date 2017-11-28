be71ffae5b9cfdc1d0c7a9bad3956514db97c096
Hasanenm <Hasanenm@ND96X912.sqs.group.intl>
2017-11-28 15:02:50 +0100

    New project

---
+++ src/test/java/MavenGitJenkinsOne/TestmeAna.java
@@ -0,0 +1,15 @@
+package MavenGitJenkinsOne;
+
+import org.testng.annotations.Test;
+
+public class TestmeAna {
+
+	@Test
+	public void testMe() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hasanenm\\Downloads\\hime\\chromedriver.exe");
+		// ChromeDriver driver = new ChromeDriver();
+		// driver.get("https://mvnrepository.com/artifact/org.testng/testng/6.13.1");
+		System.out.println("this test");
+	}
+
+}
